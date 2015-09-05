7b3e4ffe8735cd205007cc6be6161c6a6a66a82b
Roman <romitch@gmail.com>
2015-09-05 10:50:14 -0400

    Update setup.py
    version 0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 # http://www.ewencp.org/blog/a-brief-introduction-to-packaging-python/
 setup(
     name='trml2pdf',
-    version='0.2',
+    version='0.3',
     description='''Tiny RML2PDF is a tool to easily create PDF document using special HTML-like markup language. 
 It can be used as a Python library or as a standalone binary. It converts a RML, an XML dialect that lets you define the precise appearance of a printed document, to a PDF. You can use your existing tools to generate an input file  that exactly describes the layout of a printed document, and RML2PDF converts it into PDF. RML is a much more powerfull and flexible alternative to XSL:FO.
 The executable read a RML file to the standard input and output a PDF file to the standard output.''',
