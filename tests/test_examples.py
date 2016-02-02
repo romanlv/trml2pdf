5ab998e255c298f341c509a7225c7c568e91dea9
Roman L <romitch@gmail.com>
2016-02-01 21:54:54 -0500

    version 0.4

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,17 +1,17 @@
 import os
 import unittest
 
-from six import text_type
-
 import trml2pdf  # dev mode: python setup.py develop
+
+from six import text_type
 
 
 EXAMPLES_DIR = "examples"
-
+TESTS_DIR = os.path.dirname(os.path.realpath(__file__))
 
 # sys.path.append(EXAMPLES_DIR)
 
-class Test(unittest.TestCase):
+class TestExamples(unittest.TestCase):
     """run pdf genration using all files in examples."""
 
     def test_run_all(self):
@@ -32,6 +32,23 @@ class Test(unittest.TestCase):
                 with open(path, "r") as f:
                     output = trml2pdf.parseString(f.read())
                 self.assertIsNotNone(output)
+
+
+
+
+
+
+# does not work, todo find a solution
+# class TestOutput(unittest.TestCase):
+#     def test_bug8(self):
+#         with open(TESTS_DIR + '/templates/bug_8.rml', "r") as f:
+#             output = trml2pdf.parseString(f.read())
+#             print(output)
+#         self.assertNotIn('\\n', text_type(output))  # not so easy to test PDF content, it is encoded
+#         with open(TESTS_DIR + '/templates/bug8.pdf', 'wb') as w:
+#             w.write(output)
+
+
 
 
 if __name__ == "__main__":
